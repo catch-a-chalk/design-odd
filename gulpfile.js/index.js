c09183622106450108013b67f9c8edfa1b25ce08
Denis Makushin <the.makushin@yandex.ru>
2023-08-02 12:31:06 +0300

    *.json gh-pages

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,10 +1,5 @@
 const { watch } = require("gulp");
-const ghPages = require('gh-pages');
 const path = require('path');
-function deploy(cb) {
-  ghPages.publish(path.join(process.cwd(), './build'), cb);
-}
-exports.deploy = deploy;
 
 global.$ = {
   // Пакеты
