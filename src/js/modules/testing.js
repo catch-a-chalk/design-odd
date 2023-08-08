36e9d1b1523a41a96dcffeb9c2c8f2a14c3839ec
Denis Makushin <the.makushin@yandex.ru>
2023-08-08 14:14:52 +0300

    Form2 RE:Markup

--- src/js/modules/testing.js
+++ src/js/modules/testing.js
@@ -4,14 +4,16 @@ export const setDataForm = (selector, index) => {
       const getData = (form) => {
         const data = {};
   
-        // Записываем данные всех input'ов в объект data
         const inputs = form.querySelectorAll('input');
         inputs.forEach((input) => {
           if (input.name && (input.type !== 'checkbox' || input.checked)) {
             if (!data[input.name]) {
               data[input.name] = [];
             }
-            data[input.name].push(input.value);
+            data[input.name].push({
+              value: input.value,
+              span: input.dataset.span,
+            });
           }
         });
   
@@ -24,7 +26,6 @@ export const setDataForm = (selector, index) => {
         const formData = getData(form);
         let data = localStorage.getItem('data-form') ? JSON.parse(localStorage.getItem('data-form')) : {};
   
-        // Объединяем текущие данные с уже сохраненными данными в localStorage
         data[index] = { ...data[index], ...formData };
   
         localStorage.setItem('data-form', JSON.stringify(data));
@@ -35,73 +36,179 @@ export const setDataForm = (selector, index) => {
   export const getDataForm = (formSelector) => {
     const form = document.querySelector(formSelector);
     const data = JSON.parse(localStorage.getItem('data-form'));
-    const output = form.querySelector('.ordering__details ul');
+    const outputContainer = form.querySelector('.ordering__container');
     const remove = form.querySelector('.ordering__container button');
+    let totalInputs = 0;
   
     remove.addEventListener('click', () => {
       localStorage.removeItem('data-form');
-      while (output.firstChild) {
-        output.removeChild(output.firstChild);
+      while (outputContainer.firstChild) {
+        outputContainer.removeChild(outputContainer.firstChild);
       }
+      updateTotalInputs(0);
     });
   
-    let totalInputs = 0; // Переменная для общего количества инпутов
-  
     if (data && data[1]) {
-      const formData = data[1]; // Используйте нужный индекс для получения данных
+      const formData = data[1];
+      const positionsMap = new Map();
   
       for (const prop in formData) {
         if (formData.hasOwnProperty(prop)) {
           formData[prop].forEach((value) => {
-            const li = document.createElement('li');
+            if (value.span.trim() !== '' && !positionsMap.has(value.value)) {
+              positionsMap.set(value.value, value.span);
   
-            const button = document.createElement('button');
-            button.setAttribute('type', 'button');
-            const span = document.createElement('span');
-            span.classList.add('ordering__cross');
-            button.appendChild(span);
+              const label = document.createElement('label');
+              label.setAttribute('for', `form-${value.value}`);
   
-            const outputElement = document.createElement('output');
-            outputElement.textContent = value;
+              const button = document.createElement('button');
+              button.setAttribute('type', 'button');
+              button.classList.add('ordering__cross');
   
-            li.appendChild(button);
-            li.appendChild(outputElement);
+              const input = document.createElement('input');
+              input.setAttribute('type', 'text');
+              input.setAttribute('name', 'positions');
+              input.setAttribute('id', `form-${value.value}`);
+              input.value = value.value;
+              input.classList.add('visually-hidden');
   
-            output.appendChild(li);
+              const span = document.createElement('span');
+              span.textContent = value.span;
   
-            // Добавляем обработчик события для каждого .ordering__cross элемента
-            button.addEventListener('click', () => {
-              // Удаляем соответствующий инпут из localStorage
-              const data = JSON.parse(localStorage.getItem('data-form'));
-              if (data && data[1]) {
-                const index = data[1][prop].indexOf(value);
-                if (index !== -1) {
-                  data[1][prop].splice(index, 1);
-                  localStorage.setItem('data-form', JSON.stringify(data));
+              label.appendChild(button);
+              label.appendChild(input);
+              label.appendChild(span);
+              outputContainer.appendChild(label);
+  
+              button.addEventListener('click', () => {
+                const data = JSON.parse(localStorage.getItem('data-form'));
+                if (data && data[1]) {
+                  const index = data[1][prop].findIndex(item => item.value === value.value);
+                  if (index !== -1) {
+                    data[1][prop].splice(index, 1);
+                    localStorage.setItem('data-form', JSON.stringify(data));
+  
+                    outputContainer.removeChild(label);
+                    totalInputs--;
+                    updateTotalInputs(totalInputs);
+                  }
                 }
-              }
+              });
   
-              // Удаляем текущий li элемент из DOM
-              output.removeChild(li);
-  
-              // Уменьшаем значение количества инпутов при удалении
-              totalInputs--;
-              updateTotalInputs(totalInputs);
-            });
-  
-            // Увеличиваем значение количества инпутов при добавлении
-            totalInputs++;
+              totalInputs++;
+            }
           });
         }
       }
   
-      // Обновляем значение количества инпутов после завершения цикла
       updateTotalInputs(totalInputs);
     }
   };
   
-  // Функция для обновления значения количества инпутов в <h3> элементе
-  function updateTotalInputs(count) {
-    const totalInputsElement = document.querySelector('.ordering__details h3');
-    totalInputsElement.textContent = `Выбранные позиции: ${count}`;
-  }
+  
+  
+  
+  
+  
+
+//   export const setDataForm = (selector, index) => {
+//     const form = document.querySelector(selector);
+//     if (form) {
+//       const getData = (form) => {
+//         const data = {};
+  
+//         const inputs = form.querySelectorAll('input');
+//         inputs.forEach((input) => {
+//           if (input.name && (input.type !== 'checkbox' || input.checked)) {
+//             if (!data[input.name]) {
+//               data[input.name] = [];
+//             }
+//             data[input.name].push({
+//               value: input.value,
+//               span: input.dataset.span,
+//             });
+//           }
+//         });
+  
+//         return data;
+//       };
+  
+//       form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+  
+//         const formData = getData(form);
+//         let data = localStorage.getItem('data-form') ? JSON.parse(localStorage.getItem('data-form')) : {};
+  
+//         data[index] = { ...data[index], ...formData };
+  
+//         localStorage.setItem('data-form', JSON.stringify(data));
+//       });
+//     }
+//   };
+
+//   export const getDataForm = (formSelector) => {
+//     const form = document.querySelector(formSelector);
+//     const data = JSON.parse(localStorage.getItem('data-form'));
+//     const output = form.querySelector('.ordering__details ul');
+//     const remove = form.querySelector('.ordering__container button');
+  
+//     remove.addEventListener('click', () => {
+//       localStorage.removeItem('data-form');
+//       while (output.firstChild) {
+//         output.removeChild(output.firstChild);
+//       }
+//       updateTotalInputs(0); // Обновляем количество инпутов в заголовке
+//     });
+  
+//     let totalInputs = 0;
+  
+//     if (data && data[1]) {
+//       const formData = data[1];
+  
+//       for (const prop in formData) {
+//         if (formData.hasOwnProperty(prop)) {
+//           formData[prop].forEach((value) => {
+//             const li = document.createElement('li');
+//             const button = document.createElement('button');
+//             button.setAttribute('type', 'button');
+//             const span = document.createElement('span');
+//             span.classList.add('ordering__cross');
+//             button.appendChild(span);
+  
+//             const outputElement = document.createElement('output');
+//             outputElement.setAttribute('id', value.value);
+//             outputElement.textContent = value.span;
+  
+//             li.appendChild(button);
+//             li.appendChild(outputElement);
+  
+//             output.appendChild(li);
+  
+//             button.addEventListener('click', () => {
+//               const data = JSON.parse(localStorage.getItem('data-form'));
+//               if (data && data[1]) {
+//                 const index = data[1][prop].findIndex(item => item.value === value.value);
+//                 if (index !== -1) {
+//                   data[1][prop].splice(index, 1);
+//                   localStorage.setItem('data-form', JSON.stringify(data));
+  
+//                   output.removeChild(li);
+//                   totalInputs--;
+//                   updateTotalInputs(totalInputs);
+//                 }
+//               }
+//             });
+  
+//             totalInputs++;
+//           });
+//         }
+//       }
+  
+//       updateTotalInputs(totalInputs);
+//     }
+//   };
+  
+//   function updateTotalInputs(count) {
+//     const totalInputsElement = document.querySelector('.ordering__details h3');
+//     totalInputsElement.textContent = `Выбранные позиции: ${count}`;
+//   }
