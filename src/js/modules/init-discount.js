bb1cb2dbffb86138cff5791da965d649d286a0ce
Denis Makushin <the.makushin@yandex.ru>
2023-08-13 17:18:52 +0300

    Удалена картинка из футера, добавлено зацикленное видео в страницу заказов

--- src/js/modules/init-discount.js
+++ src/js/modules/init-discount.js
@@ -2,16 +2,24 @@ export const initDiscount = () => {
   const orderingDiscount = document.querySelector('.ordering__discount input');
 
   const updateDiscountValue = () => {
-    const localStorageData = JSON.parse(localStorage.getItem('data-form')); // Получаем данные из localStorage
-    const targetValue = localStorageData['2']['ready-radio'][0]['value'];
+    const localStorageData = JSON.parse(localStorage.getItem('data-form'));
+    const hasKey2 = localStorageData.hasOwnProperty('2') && localStorageData['2']['ready-radio'].length > 0;
+    const hasKey3 = localStorageData.hasOwnProperty('3') && localStorageData['3']['web'].length > 0;
 
-    if (targetValue === 'package-branding-idenity') {
+    if (hasKey2 && hasKey3) {
       orderingDiscount.setAttribute('value', '15%');
     } else {
       orderingDiscount.removeAttribute('value');
     }
   };
 
-  window.addEventListener('storage', updateDiscountValue);
+  // Добавляем обработчик события на изменение данных в localStorage в реальном времени
+  window.addEventListener('storage', (event) => {
+    if (event.key === 'data-form') {
+      updateDiscountValue();
+    }
+  });
+
+  // Вызываем функцию для начальной проверки
   updateDiscountValue();
 }
