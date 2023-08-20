354c963de9ee4896ed16e85e8df09b32cf73b592
Denis Makushin <the.makushin@yandex.ru>
2023-08-20 06:57:32 +0300

    Выделение отмеченных чекбоксов, скрипт скидки подправлен в связи с измененной разметкой

--- src/js/modules/init-discount.js
+++ src/js/modules/init-discount.js
@@ -3,10 +3,10 @@ export const initDiscount = () => {
 
   const updateDiscountValue = () => {
     const localStorageData = JSON.parse(localStorage.getItem('data-form'));
-    const hasKey7 = localStorageData.hasOwnProperty('7') && localStorageData['7']['ready-check'].length > 0;
-    const hasKey8 = localStorageData.hasOwnProperty('8') && localStorageData['8']['ready-check'].length > 0;
-    const hasKey9 = localStorageData.hasOwnProperty('9') && localStorageData['9']['ready-check'].length > 0;
-    const hasKey3 = localStorageData.hasOwnProperty('3') && localStorageData['3']['web'].length > 0;
+    const hasKey3 = localStorageData.hasOwnProperty('3') && localStorageData['3']['web'] && localStorageData['3']['web'].length > 0;
+    const hasKey7 = localStorageData.hasOwnProperty('7') && localStorageData['7']['ready-package'] && localStorageData['7']['ready-package'].length > 0;
+    const hasKey8 = localStorageData.hasOwnProperty('8') && localStorageData['8']['ready-package'] && localStorageData['8']['ready-package'].length > 0;
+    const hasKey9 = localStorageData.hasOwnProperty('9') && localStorageData['9']['ready-package'] && localStorageData['9']['ready-package'].length > 0;
 
     if ((hasKey7 || hasKey8 || hasKey9) && hasKey3) {
       orderingDiscount.setAttribute('value', '15%');
